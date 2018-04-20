c695eadd5c34c90b6baedd5eaa45e595e3c5449c
Bynow <thc19940301>
2018-04-20 18:57:26 +0800

    aop全局异常处理 @Bynow

--- rei-admin/src/main/java/com/bynow/rei/admin/aop/GlobalExceptionHandler.java
+++ rei-admin/src/main/java/com/bynow/rei/admin/aop/GlobalExceptionHandler.java
@@ -2,8 +2,7 @@ package com.bynow.rei.admin.aop;
 
 import com.bynow.rei.common.support.exception.rei.ReiException;
 import com.bynow.rei.core.base.tips.ErrorTip;
-import com.bynow.rei.core.log.LogManager;
-import com.bynow.rei.core.log.factory.LogTaskFactory;
+import com.bynow.rei.core.common.exception.BizExceptionEnum;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
@@ -12,6 +11,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import static com.bynow.rei.core.util.HttpKit.getRequest;
 
 
 /**
@@ -22,7 +23,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  */
 @ControllerAdvice
 @Order(-1)
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler{
 
     private Logger log = LoggerFactory.getLogger(this.getClass());
 
@@ -33,9 +34,20 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
     public ErrorTip notFount(ReiException e) {
-        //LogManager.me().executeLog(LogTaskFactory.exceptionLog(ShiroKit.getUser().getId(), e));
-        //getRequest().setAttribute("tip", e.getMessage());
+        getRequest().setAttribute("tip", e.getMessage());
         log.error("业务异常:", e);
         return new ErrorTip(e.getCode(), e.getMessage());
+    }
+
+    /**
+     * 拦截未知的运行时异常
+     */
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ErrorTip notFount(RuntimeException e) {
+        getRequest().setAttribute("tip", "服务器未知运行时异常");
+        log.error("运行时异常:", e);
+        return new ErrorTip(BizExceptionEnum.SERVER_ERROR.getCode(), BizExceptionEnum.SERVER_ERROR.getMessage());
     }
 }
