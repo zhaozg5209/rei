5f18d2847b31385aa9a5ed4a3d7681d555956cf5
Bynow <thc19940301>
2018-04-20 18:17:36 +0800

    aop  generator @Bynow

---
+++ rei-common/src/main/java/com/bynow/rei/common/support/exception/rei/ServiceExceptionEnum.java
@@ -0,0 +1,16 @@
+package com.bynow.rei.common.support.exception.rei;
+
+
+
+public interface ServiceExceptionEnum {
+
+    /**
+     * 获取异常编码
+     */
+    Integer getCode();
+
+    /**
+     * 获取异常信息
+     */
+    String getMessage();
+}
---
+++ rei-core/src/java/com/bynow/rei/core/aop/BaseControllerExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.bynow.rei.core.aop;
+
+
+import com.bynow.rei.common.support.exception.rei.ReiException;
+import com.bynow.rei.common.support.exception.rei.ReiExceptionEnum;
+import com.bynow.rei.core.base.tips.ErrorTip;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * 全局的的异常拦截器（拦截所有的控制器）（带有@RequestMapping注解的方法上都会拦截）
+ *
+ * @author fengshuonan
+ * @date 2016年11月12日 下午3:19:56
+ */
+public class BaseControllerExceptionHandler {
+
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * 拦截业务异常
+     */
+    @ExceptionHandler(ReiException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ErrorTip notFount(ReiException e) {
+        log.error("业务异常:", e);
+        return new ErrorTip(e.getCode(), e.getMessage());
+    }
+
+    /**
+     * 拦截未知的运行时异常
+     */
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ErrorTip notFount(RuntimeException e) {
+        log.error("运行时异常:", e);
+        return new ErrorTip(ReiExceptionEnum.SERVER_ERROR.getCode(), ReiExceptionEnum.SERVER_ERROR.getMessage());
+    }
+
+}
