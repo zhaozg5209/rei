db9d0e95f8cb5c085f27c425c96d645476f726d3
Bynow <thc19940301>
2018-04-20 18:35:12 +0800

    框架搭建 @Bynow

--- rei-admin/src/main/java/com/bynow/rei/admin/aop/PermissionAop.java
+++ rei-admin/src/main/java/com/bynow/rei/admin/aop/PermissionAop.java
@@ -1,37 +1,37 @@
-///**
-// * Copyright (c) 2015-2017, Chill Zhuang 庄骞 (smallchill@163.com).
-// * <p>
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// * <p>
-// * http://www.apache.org/licenses/LICENSE-2.0
-// * <p>
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
-//package com.bynow.rei.admin.aop;
-//
-//
-//import org.aspectj.lang.annotation.Aspect;
-//import org.aspectj.lang.annotation.Pointcut;
-//import org.springframework.core.annotation.Order;
-//import org.springframework.stereotype.Component;
-//
-///**
-// * AOP 权限自定义检查
-// */
-//@Aspect
-//@Component
-//@Order(200)
-//public class PermissionAop {
-//
-//    @Pointcut(value = "@annotation(com.stylefeng.guns.core.common.annotion.Permission)")
-//    private void cutPermission() {
-//
-//    }
-//
-//}
+/**
+ * Copyright (c) 2015-2017, Chill Zhuang 庄骞 (smallchill@163.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.bynow.rei.admin.aop;
+
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * AOP 权限自定义检查
+ */
+@Aspect
+@Component
+@Order(200)
+public class PermissionAop {
+
+    @Pointcut(value = "@annotation(com.bynow.rei.core.common.annotion.Permission)")
+    private void cutPermission() {
+
+    }
+
+}
