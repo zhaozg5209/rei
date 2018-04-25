342e375b1c4da31fe2c9bef9e9efa0d89a1942e5
Bynow <thc19940301>
2018-04-25 14:00:01 +0800

    generator @Bynow

--- rei-core/src/java/com/bynow/rei/core/module/service/test/TestService.java
+++
@@ -1,9 +0,0 @@
-package com.bynow.rei.core.module.service.test;
-
-import com.bynow.rei.core.module.entity.test.Test;
-
-import java.util.List;
-
-public interface TestService {
-    List<Test> test();
-}
