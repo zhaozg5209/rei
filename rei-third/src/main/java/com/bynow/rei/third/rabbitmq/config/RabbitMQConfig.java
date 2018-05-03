a5cf3170132b97de4abf549e42c6398265c14733
Bynow(伯言) <1923184056@qq.com>
2018-05-03 15:57:29 +0800

    delete

--- rei-third/src/main/java/com/bynow/rei/third/rabbitmq/config/RabbitMQConfig.java
+++ rei-third/src/main/java/com/bynow/rei/third/rabbitmq/config/RabbitMQConfig.java
@@ -1,57 +1,58 @@
 package com.bynow.rei.third.rabbitmq.config;
 
-import com.bynow.rei.third.rabbitmq.util.RabbitUtil;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by IntelliJ IDEA.
  *
  * @Author: bynow
  * @Description:
- * @Date: created in 15:36 2018/4/30
+ * @Date: created in 14:21 2018/5/3
  */
-
 @Configuration
-public class RabbitMQConfig {
-    /**
-     * 注入配置文件属性
-     */
-    @Value("${spring.rabbitmq.addresses}")
-    String addresses;//MQ地址
-    @Value("${spring.rabbitmq.username}")
-    String username;//MQ登录名
-    @Value("${spring.rabbitmq.password}")
-    String password;//MQ登录密码
-    @Value("${spring.rabbitmq.virtual-host}")
-    String vHost;//MQ的虚拟主机名
+public class RabbitMqConfig {
 
+    @Value("${rabbitmq.addresses}")
+    private String addresses;
+    @Value("${rabbitmq.username}")
+    private String username;
+    @Value("${rabbitmq.password}")
+    private String password;
+    @Value("${rabbitmq.publisher-confirms}")
+    private Boolean publisherConfirms;
+    @Value("${rabbitmq.virtual-host}")
+    private String virtualHost;
 
-    /**
-     * 创建 ConnectionFactory
-     *
-     * @return
-     * @throws Exception
-     */
+    // 构建mq实例工厂
     @Bean
-    public ConnectionFactory connectionFactory() throws Exception {
-        return RabbitUtil.connectionFactory(addresses, username, password, vHost);
+    public ConnectionFactory connectionFactory(){
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setAddresses(addresses);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        connectionFactory.setPublisherConfirms(publisherConfirms);
+        connectionFactory.setVirtualHost(virtualHost);
+        return connectionFactory;
     }
 
-    /**
-     * 创建 RabbitAdmin
-     *
-     * @param connectionFactory
-     * @return
-     * @throws Exception
-     */
     @Bean
-    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) throws Exception {
-        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
-        return rabbitAdmin;
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory){
+        return new RabbitAdmin(connectionFactory);
     }
 
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public RabbitTemplate rabbitTemplate(){
+        RabbitTemplate template = new RabbitTemplate(connectionFactory());
+        return template;
+    }
 }
